79b8311bf19a9c07b94af85251d86549efeee439
Phil7789 <87429599+Phil7789@users.noreply.github.com>
2023-12-27 15:33:12 +0100

    Moved fplnInfo struct

--- flightplan.h
+++ flightplan.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "pch.h"
 #include "EuroScopePlugIn.h"
+#include "sid.h"
 
 #include <string>
 #include <vector>
@@ -9,6 +10,12 @@ namespace vsid
 {
 	namespace fpln
 	{
+		struct info
+		{
+			bool atcRWY = false;
+			vsid::sids::sid sid = {};
+			vsid::sids::sid customSid = {};
+		};
 		/**
 		 * @brief Strip the filed route from SID/RWY and/or SID to have a bare route to populate with set SID.
 		 * Any SIDs or RWYs will be deleted and have to be reset.
@@ -28,5 +35,11 @@ namespace vsid
 		 * @return
 		 */
 		std::pair<std::string, std::string> getAtcBlock(const std::vector<std::string>& filedRoute, const std::string origin);
+
+		/*bool findRemarks(const EuroScopePlugIn::CFlightPlanData& fplnData, const std::string(& searchStr));
+
+		void removeRemark(EuroScopePlugIn::CFlightPlanData& fplnData, const std::string(&toRemove));
+
+		void addRemark(EuroScopePlugIn::CFlightPlanData& fplnData, const std::string(& toAdd));*/
 	}
 }
