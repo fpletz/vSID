81323e421e36b435c13a98609ae5536238da8f24
Phil7789 <87429599+Phil7789@users.noreply.github.com>
2024-02-02 14:09:13 +0100

    Removed old version

--- sid.h
+++ sid.h
@@ -43,7 +43,6 @@ namespace vsid
 		int lvp;
 		int timeFrom;
 		int timeTo;
-
 		/**
 		 * @brief Gets the SID name (wpt + number + designator)
 		 *
@@ -80,78 +79,4 @@ namespace vsid
 		 */
 		bool operator!=(const Sid& sid);
 	};
-	//namespace sids
-	//{
-	//	struct Point
-	//	{
-	//		float lat;
-	//		float lon;
-	//	};
-
-	//	struct SIDArea
-	//	{
-	//		Point P1;
-	//		Point P2;
-	//		Point P3;
-	//		Point P4;
-	//	};
-
-	//	struct sid
-	//	{
-	//		std::string waypoint = "";
-	//		char number = ' ';
-	//		std::string designator = "";
-	//		std::string rwy = "";
-	//		int initialClimb = 0;
-	//		bool climbvia = false;
-	//		int prio = 0;
-	//		bool pilotfiled = false;
-	//		std::string wtc = "";
-	//		std::string engineType = "";
-	//		std::map<std::string, bool> acftType = {};
-	//		int engineCount = 0;
-	//		int mtow = 0;
-	//		std::string customRule = "";
-	//		SIDArea area = {};
-	//		std::string equip = "";
-	//		int lvp = -1;
-	//		int timeFrom = -1;
-	//		int timeTo = -1;
-	//	};
-	//	/**
-	//	 * @brief Gets the SID name (wpt + number + designator)
-	//	 * 
-	//	 * @param sid - the sid object to check
-	//	 */
-	//	std::string getName(const sid& sid);
-	//	/**
-	//	 * @brief Gets the runway associated with a sid
-	//	 * 
-	//	 * @param sid - the sid object
-	//	 * @return runway if single or first runway if multiple
-	//	 */
-	//	std::string getRwy(const sid& sid);
-	//	/**
-	//	 * @brief Checks if a SID object is empty (waypoint is checked)
-	//	 * 
-	//	 * @param sid - the sid object
-	//	 */
-	//	bool isEmpty(const sid& sid);
-	//	/**
-	//	 * @brief Compares if two SIDs are the same
-	//	 * 
-	//	 * @param sid1 - first sid to compare
-	//	 * @param sid2 - second sid to compare
-	//	 * @return true - if waypoint, number and designator match
-	//	 */
-	//	bool operator==(const sid& sid1, const sid& sid2);
-	//	/**
-	//	 * @brief Compares if two SIDs are the different
-	//	 * 
-	//	 * @param sid1 - first sid to compare
-	//	 * @param sid2 - second sid to compare
-	//	 * @return true - if at least one of waypoint, number or designator don't match
-	//	 */
-	//	bool operator!=(const sid& sid1, const sid& sid2);
-	//}
 }
