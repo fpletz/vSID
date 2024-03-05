5d85bcadab9453d58f40b3d06e9d71fca928a1bc
Phil7789 <87429599+Phil7789@users.noreply.github.com>
2024-03-05 12:55:57 +0100

    Added minor error handling

--- messageHandler.cpp
+++ messageHandler.cpp
@@ -11,11 +11,19 @@ vsid::MessageHandler::~MessageHandler() { this->closeConsole(); }
 
 void vsid::MessageHandler::writeMessage(std::string sender, std::string msg)
 {
-	if (this->currentLevel == Level::Debug && sender == "DEBUG")
+	try
 	{
-		std::cout << sender << ": " << msg << '\n';
+		if (this->currentLevel == Level::Debug && sender == "DEBUG")
+		{
+			std::cout << sender << ": " << msg << '\n';
+		}
+		else if (/*this->currentLevel != Level::Debug && */sender != "DEBUG") this->msg.push_back(std::pair<std::string, std::string>(sender, msg));
 	}
-	else this->msg.push_back(std::pair<std::string, std::string>(sender, msg));
+	catch (std::exception& e)
+	{
+		this->msg.push_back(std::pair<std::string, std::string>("ERROR", e.what()));
+	}
+	
 }
 
 std::pair<std::string, std::string> vsid::MessageHandler::getMessage()
@@ -41,14 +49,29 @@ void vsid::MessageHandler::dropMessage()
 
 void vsid::MessageHandler::openConsole()
 {
-	AllocConsole();
-	freopen_s(&this->consoleFile, "CONOUT$", "w", stdout);
+	try
+	{
+		AllocConsole();
+		freopen_s(&this->consoleFile, "CONOUT$", "w", stdout);
+	}
+	catch (std::exception& e)
+	{
+		this->msg.push_back(std::pair<std::string, std::string>("ERROR", e.what()));
+	}
 }
 
 void vsid::MessageHandler::closeConsole()
 {
-	fclose(this->consoleFile);
-	FreeConsole();
+	try
+	{
+		fclose(this->consoleFile);
+		FreeConsole();
+	}
+	catch (std::exception& e)
+	{
+		this->msg.push_back(std::pair<std::string, std::string>("ERROR", e.what()));
+	}
+	
 }
 
 int vsid::MessageHandler::getLevel() const
